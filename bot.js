4debc433d18eac18cfcbc44bf183ea55ca01ac0f
andreyrunov <elefant86@bk.ru>
2022-12-08 12:21:57 +0300

    add reply to user

--- bot.js
+++ bot.js
@@ -1,6 +1,32 @@
 const { Telegraf } = require('telegraf')
 const axios = require('axios')
+const puppeteer = require('puppeteer')
 require('dotenv').config()
+
+async function getLocationName(url) {
+	// const getLocationName = async (url) => {
+	// открываем браузер
+	const browser = await puppeteer.launch()
+
+	// открываем вкладку в браузере
+	const page = await browser.newPage()
+
+	// переходим по url адресу
+	await page.goto(`${url}`)
+
+	// делаем скриншот страницы для проверки правильности отработки программы
+	// await page.screenshot({ path: 'example.png' })
+
+	// в браузере находим нужный элемент и копируем (прав.кнопка мыши) его селектор
+	const locationName = await page.$eval('#main_title', (el) => el.innerText)
+
+	// закрываем браузер
+	await browser.close()
+
+	if (locationName) {
+		return locationName
+	}
+}
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) =>
@@ -15,7 +41,7 @@ bot.start((ctx) =>
 // обработчик, который принимает от пользователя сообщение и выводит его в консоль
 bot.on('message', async (ctx) => {
 	if (ctx.message.location) {
-		console.log(ctx.message)
+		// console.log(ctx.message)
 		const url = `https://api.weather.yandex.ru/v2/informers?lat=${ctx.message.location.latitude}&lon=${ctx.message.location.longitude}`
 
 		try {
@@ -28,10 +54,26 @@ bot.on('message', async (ctx) => {
 					// 'Content-Type': 'application/x-www-form-urlencoded',
 				},
 			})
+
 			console.log(
 				response.data,
 				'=============================================================='
 			)
+
+			// const dataTime = new Date(response.data.now_dt)
+			const dataTime = new Date()
+			// console.log(dataTime, '<----------------------------')
+
+			const location = await getLocationName(response.data.info.url)
+			// console.log(location, '<---------------------')
+			if (location) {
+				ctx.reply(`Ваше местоположение: ${location}`)
+				ctx.reply(`Сегодня: ${dataTime.getDate()}-${dataTime.getMonth()}-${dataTime.getFullYear()}, ${dataTime.getHours()}:${dataTime.getMinutes()}
+Температура: ${response.data.fact.temp}
+Ощущается как: ${response.data.fact.feels_like}
+Скорость ветра: ${response.data.fact.wind_speed}
+`)
+			}
 			// console.log(response.data.forecast.parts)
 		} catch (err) {
 			console.error(err)
